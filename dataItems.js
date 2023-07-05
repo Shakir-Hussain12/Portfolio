b5384165a57187dda3e74448dcfae38af2152ead
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-07-05 17:50:18 +0500

    updated Data items

--- dataItems.js
+++ dataItems.js
@@ -1,7 +1,7 @@
 const projs = [
   {
     title: 'Space Traveler Hub',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    info: 'This is a web application that displays commercial and scientific space travel services. The application fetches real live data from the SpaceX API. The application allows users to interact with it by booking rockets and joining selected space missions. It was built using React.js, Redux-toolkit, Bootstrap, and Rest APIs.',
     tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
     images: [
       './images/SnapshootPortfolio.png',
@@ -9,8 +9,8 @@ const projs = [
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/React-Redux-Group-Project-Space-Travelers-Hub',
+        'https://spacetravelershub-react.onrender.com',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -20,7 +20,7 @@ const projs = [
   },
   {
     title: 'To-Do List',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    info: 'Simple, Free and easy to use project designed with HTML, CSS, and JS that can be used to plan your day-to-day activities.',
     tech: ['HTML', 'CSS', 'JavaScript'],
     images: [
       './images/SnapshootPortfolio-2.png',
@@ -28,8 +28,8 @@ const projs = [
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/to-do_List',
+        'shakir-hussain12.github.io/to-do_List/',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -39,7 +39,7 @@ const projs = [
   },
   {
     title: 'UpSouly',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    info: 'Fully Responsive Concert Planner webpage designed using HTML, CSS, and JS.',
     tech: ['HTML', 'CSS', 'JavaScript'],
     images: [
       './images/SnapshootPortfolio-1.png',
@@ -47,8 +47,8 @@ const projs = [
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/Capstone1',
+        'https://shakir-hussain12.github.io/Capstone1/',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -57,17 +57,17 @@ const projs = [
     ],
   },
   {
-    title: 'BookStore',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tech: ['HTML', 'CSS'],
+    title: 'BookStore CMS',
+    info: 'Simple bookstore to add, show, and delete books.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'Redux', 'Vite'],
     images: [
       './images/SnapshootPortfolio-3.png',
       './desktop-images/SnapshootPortfolio-3.png',
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/React-Bookstore',
+        'https://jolly-clafoutis-fc42c6.netlify.app',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -77,16 +77,16 @@ const projs = [
   },
   {
     title: 'Quran Companion',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    info: "Quran Companion is a web application that displays chapters and their verses from the Holy Quran. The application fetches real live data from the Quran.API,and allows users to interact with it by accessing chapters of their interest and then verses from the same Chapters. Hopefully, I'll also add audio clips as well in the future. It was built using React.js, Redux-toolkit, Bootstrap, React-Bootstrap, and Rest APIs.",
+    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Bootstrap', 'Axios'],
     images: [
       './images/SnapshootPortfolio-3.png',
       './desktop-images/SnapshootPortfolio-3.png',
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/React-Capstone',
+        'https://wondrous-belekoy-4098f1.netlify.app',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -96,19 +96,58 @@ const projs = [
   },
   {
     title: 'Book Library',
-    info: 'Simple Book Library where user can comment on their favorite books and also like them as well',
-    tech: ['HTML', 'CSS', 'JavaScript', 'React'],
+    info: 'Library is a web application that allows users to like and comment on books.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs'],
     images: [
       './images/SnapshootPortfolio-3.png',
       './desktop-images/SnapshootPortfolio-3.png',
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/library',
+        'https://shasherazi.github.io/library/',
       ],
       [
         './desktop-images/Live_btn.png',
+        './desktop-images/Source_Btn.png',
+      ],
+    ],
+  },
+  {
+    title: 'SnapScan',
+    info: 'Interactive app that allows users to first register/login and then add/delete different categories and games within each one categories along with their prices.',
+    tech: ['HTML', 'CSS', 'Rails', 'Ruby', 'postgresql', 'Materialize'],
+    images: [
+      './images/SnapshootPortfolio-3.png',
+      './desktop-images/SnapshootPortfolio-3.png',
+    ],
+    buttons: [
+      [
+        'https://github.com/Shakir-Hussain12/Snapscan',
+        'https://snapscan.onrender.com',
+      ],
+      [
+        './desktop-images/Live_btn.png',
+        './desktop-images/Source_Btn.png',
+      ],
+    ],
+  },
+  {
+    title: 'Car-BnB',
+    info: 'Full-Stack App that was built in collaboration with 3 other members using Redux as management tool, React as Front-end and Ruby as Back-end. The application was built as 2 separate apps where Ruby on Rails was used to serve as an API for React to consume and interact with the database to add/remove/reserve cars.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs', 'Ruby', 'Rails', 'Redux'],
+    images: [
+      './images/SnapshootPortfolio-3.png',
+      './desktop-images/SnapshootPortfolio-3.png',
+    ],
+    buttons: [
+      [
+        'https://github.com/Ogaga01/Car-BnB-API',
+        'https://github.com/Lancelot-SO/carbnb-frontend',
+      ],
+      [
+        // './desktop-images/Live_btn.png',
+        './desktop-images/Source_Btn.png',
         './desktop-images/Source_Btn.png',
       ],
     ],
--- miniData.js
+++ miniData.js
@@ -1,7 +1,7 @@
 const projs = [
   {
     title: 'Space Traveler Hub',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    info: 'This is a web application that displays commercial and scientific space travel services. The application fetches real live data from the SpaceX API. The application allows users to interact with it by booking rockets and joining selected space missions. It was built using React.js, Redux-toolkit, Bootstrap, and Rest APIs.',
     tech: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
     images: [
       './images/SnapshootPortfolio.png',
@@ -9,8 +9,8 @@ const projs = [
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/React-Redux-Group-Project-Space-Travelers-Hub',
+        'https://spacetravelershub-react.onrender.com',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -19,17 +19,17 @@ const projs = [
     ],
   },
   {
-    title: 'To-Do List',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
+    title: 'SnapScan',
+    info: 'Interactive app that allows users to first register/login and then add/delete different categories and games within each one categories along with their prices.',
+    tech: ['HTML', 'CSS', 'Rails', 'Ruby', 'postgresql', 'Materialize'],
     images: [
-      './images/SnapshootPortfolio-2.png',
-      './desktop-images/SnapshootPortfolio-1.png',
+      './images/SnapshootPortfolio-3.png',
+      './desktop-images/SnapshootPortfolio-3.png',
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Shakir-Hussain12/Snapscan',
+        'https://snapscan.onrender.com',
       ],
       [
         './desktop-images/Live_btn.png',
@@ -38,20 +38,21 @@ const projs = [
     ],
   },
   {
-    title: 'UpSouly',
-    info: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
+    title: 'Car-BnB',
+    info: 'Full-Stack App that was built in collaboration with 3 other members using Redux as management tool, React as Front-end and Ruby as Back-end. The application was built as 2 separate apps where Ruby on Rails was used to serve as an API for React to consume and interact with the database to add/remove/reserve cars.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'React', 'APIs', 'Ruby', 'Rails', 'Redux'],
     images: [
-      './images/SnapshootPortfolio-1.png',
-      './desktop-images/SnapshootPortfolio-2.png',
+      './images/SnapshootPortfolio-3.png',
+      './desktop-images/SnapshootPortfolio-3.png',
     ],
     buttons: [
       [
-        'https://shakir-hussain12.github.io/Mobile-Portfolio/',
-        'https://github.com/Shakir-Hussain12/Mobile-Portfolio',
+        'https://github.com/Ogaga01/Car-BnB-API',
+        'https://github.com/Lancelot-SO/carbnb-frontend',
       ],
       [
-        './desktop-images/Live_btn.png',
+        // './desktop-images/Live_btn.png',
+        './desktop-images/Source_Btn.png',
         './desktop-images/Source_Btn.png',
       ],
     ],
