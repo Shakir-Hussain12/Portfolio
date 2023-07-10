917e631f070884fe6d859931db18af0bdf25a003
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-07-10 16:24:02 +0500

    fixed Link to Projects

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import dataItems from '../../miniData';
 import WorkItem from './workItem';
 
-// function isElementInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   return (
-//     rect.top >= 0
-//     && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-//   );
-// }
-
-// function handleScroll() {
-//   const projects = document.getElementsByClassName('work-item');
-//   const button = document.querySelector('#expand');
-//   for (let i = 0; i < projects.length; i += 1) {
-//     const project = projects[i];
-//     if (isElementInViewport(project) && !project.classList.contains('animate') && project && button) {
-//       project.classList.remove('inactive');
-//       button.classList.remove('inactive');
-//       button.classList.add('animate');
-//       project.classList.add('animate');
-//     }
-//   }
-// }
-
 export default function Works() {
   let mykey = 0;
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-  // }, []);
   return (
     <section id="work">
       {
@@ -41,10 +17,10 @@ export default function Works() {
         })
       }
       <center>
-        <a href="/project" data-aos="fade-left" className="waves-effect waves-light btn teal darken-4">
+        <NavLink to="/project" data-aos="fade-left" className="waves-effect waves-light btn teal darken-4">
           <i className="material-icons right">featured_play_list</i>
           Click to See All Projects
-        </a>
+        </NavLink>
       </center>
     </section>
   );
